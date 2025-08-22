990589031708c39210f5000591850d27cb01e00a
Suvendu Dash Adhikary <159238155+Suv2001@users.noreply.github.com>
2025-08-22 11:18:53 +0530

    Fix typo in build completion message in buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,5 +2,5 @@ def call(String imageName, String tag='latest') {
     echo "Building for Deployment"
     // Building the image with default tag if not specified
     sh "docker build -t ${imageName}:${tag} ."
-    echo "Build Complete"
+    echo "Build Completed Successfully."
 }
